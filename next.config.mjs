83344cc666f0c7f13815dd35eaf014f0bb0f1049
Tammarak <koonkoonpond@gmail.com>
2026-01-28 13:29:42 +0700

    fix: remove invalid serverActions config for Vercel build

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverActions: {
-    allowedOrigins: ['localhost:3000', '192.168.1.163:3000'],
-  },
+  // serverActions: {
+  //   allowedOrigins: ['localhost:3000', '192.168.1.163:3000'],
+  // },
 };
 
 export default nextConfig;
